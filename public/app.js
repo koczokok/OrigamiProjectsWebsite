e2b185b0c494103703d9ece31af7689126db3f33
Koczokok <p.kurylowicz@outlook.com>
2022-12-17 13:22:26 +0100

    OR-4 grid layout

--- public/app.js
+++ public/app.js
@@ -5,7 +5,17 @@ const burger = document.querySelector("#burger")
 const sectionOne = document.querySelector(".one")
 const nav = document.querySelector(".nav")
 const li = document.querySelectorAll(".mobile-nav-links li")
+const img = document.querySelector("#bimg")
 
+let height = nav.offsetHeight
+console.log(height)
+img.style.marginTop = height
+
+
+window.addEventListener('resize', function(event){
+  let height = nav.offsetHeight
+  img.style.marginTop = height
+});
 
 btn.addEventListener('click', ()=>{
     mobile.classList.toggle('is-active')
@@ -35,7 +45,7 @@ const sectionOneOptions = {
       if (!entry.isIntersecting) {
         nav.classList.add("scrolled-1");
       } else {
-        mav.classList.remove("scrolled-1");
+        nav.classList.remove("scrolled-1");
       }
     });
   },
@@ -50,8 +60,8 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'}})
 tl.to(".text", {y:'0%', duration: 1.5, stagger: 0.25})
 tl.to(".intro", {y:'-100%', duration: 1, delay: 0.5})
 tl.to(".intro", {display:'none'})
-tl.fromTo("#bimg", {opacity: 0}, {opacity: 0.5, duration: 0.5}, "-=0.5")
-tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1})
+tl.fromTo("#bimg", {opacity: 0}, {opacity: 1, duration: 0.75})
+tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1}, "-=0.5")
 tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 0.5}, "-=0.5")
 
 
