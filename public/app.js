fadac67879541199c90f81a42506d8d757e93d6c
koczokok <74832149+koczokok@users.noreply.github.com>
2022-12-23 16:45:41 +0100

    Revert "Develop"

--- public/app.js
+++ public/app.js
@@ -5,17 +5,7 @@ const burger = document.querySelector("#burger")
 const sectionOne = document.querySelector(".one")
 const nav = document.querySelector(".nav")
 const li = document.querySelectorAll(".mobile-nav-links li")
-const img = document.querySelector("#bimg")
 
-let height = nav.offsetHeight
-console.log(height)
-img.style.marginTop = height
-
-
-window.addEventListener('resize', function(event){
-  let height = nav.offsetHeight
-  img.style.marginTop = height
-});
 
 btn.addEventListener('click', ()=>{
     mobile.classList.toggle('is-active')
@@ -45,7 +35,7 @@ const sectionOneOptions = {
       if (!entry.isIntersecting) {
         nav.classList.add("scrolled-1");
       } else {
-        nav.classList.remove("scrolled-1");
+        mav.classList.remove("scrolled-1");
       }
     });
   },
@@ -60,9 +50,7 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'}})
 tl.to(".text", {y:'0%', duration: 1.5, stagger: 0.25})
 tl.to(".intro", {y:'-100%', duration: 1, delay: 0.5})
 tl.to(".intro", {display:'none'})
-tl.fromTo("#bimg", {opacity: 0}, {opacity: 0.5, duration: 0.75})
-tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1}, "-=0.5")
+tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1})
 tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 0.5}, "-=0.5")
-
 
 
